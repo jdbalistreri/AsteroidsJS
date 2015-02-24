84cdde489894d3cd0b6825b16add5f557c725005
Joe Balistreri <Jdbalistreri@gmail.com>
2015-02-23 20:22:06 -0500

    move ship triangle to be centered on hitbox

--- asteroids/lib/ship.js
+++ asteroids/lib/ship.js
@@ -7,7 +7,7 @@
 
   Asteroids.Ship = function (properties) {
     var COLOR = "#FF0000";
-    var RADIUS = 50;
+    var RADIUS = 25;
     this.speed = 0;
     this.theta = -(Math.PI / 2)
     this.facing = this.theta
@@ -36,13 +36,36 @@
     ctx.fillStyle = this.color;
     ctx.beginPath();
 
-    ctx.moveTo(this.pos[0], this.pos[1])
+    var tipPos = [this.pos[0], this.pos[1]]
+    var offsetVec = Asteroids.Util.makeVec(this.facing, 25)
+
+    ctx.moveTo(tipPos[0] + offsetVec[0], tipPos[1] + offsetVec[1])
     var vec1 = Asteroids.Util.makeVec(this.facing - 2.8, 40)
+    vec1[0] = vec1[0] + offsetVec[0]
+    vec1[1] = vec1[1] + offsetVec[1]
     ctx.lineTo(vec1[0] + this.pos[0], vec1[1] + this.pos[1])
 
     var vec2 = Asteroids.Util.makeVec(this.facing + 2.8, 40)
+    vec2[0] = vec2[0] + offsetVec[0]
+    vec2[1] = vec2[1] + offsetVec[1]
     ctx.lineTo(vec2[0] + this.pos[0], vec2[1] + this.pos[1])
+
     ctx.fill();
+    ctx.closePath();
+
+    // THIS CODE SHOWS THE HIT BOX
+    // ctx.beginPath();
+    // ctx.arc(
+    //   this.pos[0],
+    //   this.pos[1],
+    //   this.radius,
+    //   0,
+    //   2 * Math.PI,
+    //   false
+    // );
+    // ctx.strokeStyle = 'rgb(0,128,0)';
+    // ctx.lineWidth = 5;
+    // ctx.stroke();
   }
 
   Asteroids.Ship.prototype.fireBullet = function() {
